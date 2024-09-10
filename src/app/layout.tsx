8047ebe8da0ae27712cb99bc4480195ad0067266
Daniel Ryu <40980993+dryu99@users.noreply.github.com>
2024-09-10 00:55:45 -0700

    setup styles + tsconfig + home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Nunito } from "next/font/google";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+export const nunito = Nunito({
+  subsets: ["latin"],
+  weight: "400",
+  display: "swap",
+  preload: true,
 });
 
 export const metadata: Metadata = {
@@ -25,10 +21,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${nunito.className} flex h-svh flex-col antialiased`}>
+        <main className="relative flex-grow p-12">{children}</main>
       </body>
     </html>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,45 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      textColor: {
+        primary: "var(--color-text-primary)",
+        secondary: "var(--color-text-secondary)",
+        tertiary: "var(--color-text-tertiary)",
+        icon: "var(--color-text-icon)",
+        code: "var(--color-text-code)",
+        btn: "var(--color-text-btn)",
+        link: "var(--color-text-link)",
+        placeholder: "var(--color-text-placeholder)",
+      },
+      backgroundColor: {
+        primary: "var(--color-bg-primary)",
+        secondary: "var(--color-bg-secondary)",
+        tertiary: "var(--color-bg-tertiary)",
+        brand: "var(--color-bg-brand)",
+        hover: "var(--color-bg-hover)",
+        code: "var(--color-bg-code)",
+        tooltip: "var(--color-bg-tooltip)",
+        op: "var(--color-bg-op)",
+        btn: "var(--color-bg-btn)",
+      },
+      borderColor: {
+        primary: "var(--color-border-primary)",
+        secondary: "var(--color-border-secondary)",
+        focus: "var(--color-border-focus)",
+      },
+      utilities: {
+        ".line-clamp-2": {
+          display: "-webkit-box",
+          "-webkit-box-orient": "vertical",
+          "-webkit-line-clamp": "2",
+          overflow: "hidden",
+        },
       },
     },
   },
