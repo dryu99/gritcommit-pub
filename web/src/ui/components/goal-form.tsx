29191000f62a671fbeb32442eba73164021c8fac
Daniel Ryu <40980993+dryu99@users.noreply.github.com>
2024-12-07 16:01:27 -0800

    adjust select width + add constraints on stake input

--- web/src/ui/components/goal-form.tsx
+++ web/src/ui/components/goal-form.tsx
@@ -119,6 +119,8 @@ export const GoalForm = () => {
             id="stakeAmount"
             name="stakeAmount"
             type="number"
+            min="0"
+            step="0.01"
             className="p-2 rounded-md border"
             placeholder="20"
           />
@@ -161,7 +163,7 @@ export const GoalForm = () => {
                 id="dueDate"
                 name="dueDate"
                 type="date"
-                className="p-2 rounded-md border w-1/2"
+                className="w-[177px] p-2 rounded-md border"
                 onChange={(e) => setHasDate(!!e.target.value)}
               />
               {hasDate && <span className="text-gray-500">11:59pm</span>}
@@ -180,7 +182,7 @@ export const GoalForm = () => {
                   id="recurringType"
                   name="recurringType"
                   className={cn(
-                    "h-10 p-2 rounded-md border appearance-none w-1/2",
+                    "w-[177px] h-10 p-2 rounded-md border appearance-none",
                     "bg-[url('data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%23131313%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E')]",
                     "bg-[length:0.7em] bg-[right_0.7rem_center] bg-no-repeat pr-8"
                   )}
@@ -280,9 +282,9 @@ export const GoalForm = () => {
 
       <button
         type="submit"
-        className="bg-orange-600 text-white p-2 rounded-md hover:bg-orange-700"
+        className="mt-6 bg-orange-600 text-white p-2 rounded-md hover:bg-orange-700"
       >
-        Create Commitment
+        Commit
       </button>
       {errorMessage && <p className="text-red-500">{errorMessage}</p>}
     </form>
