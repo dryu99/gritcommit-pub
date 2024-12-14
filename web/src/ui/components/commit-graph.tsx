6625991c8d24fd79e42c5ae03b3b3f247455485c
Daniel Ryu <40980993+dryu99@users.noreply.github.com>
2024-12-14 14:49:09 -0800

    use utc dates more

--- web/src/ui/components/commit-graph.tsx
+++ web/src/ui/components/commit-graph.tsx
@@ -157,14 +157,14 @@ const toCommitSquares = (dates: Date[]): CommitSquare[] => {
   const commitSquares = Array.from(
     { length: DAYS_IN_CURRENT_YEAR },
     (_, i) => ({
-      date: new Date(CURRENT_YEAR, 0, i + 1),
+      date: new Date(Date.UTC(CURRENT_YEAR, 0, i + 1)),
       commits: 0,
     }),
   );
 
   for (const date of dates) {
     const dayOfYear = Math.floor(
-      (date.getTime() - new Date(CURRENT_YEAR, 0, 1).getTime()) /
+      (date.getTime() - new Date(Date.UTC(CURRENT_YEAR, 0, 1)).getTime()) /
         (1000 * 60 * 60 * 24),
     );
     const commitSquare = commitSquares[dayOfYear];
