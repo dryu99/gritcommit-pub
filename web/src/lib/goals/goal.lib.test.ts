19b6fb80f55fef79f26964fe8333a4df59fda11d
Daniel Ryu <40980993+dryu99@users.noreply.github.com>
2024-12-16 15:22:40 -0800

    write more goal tests

--- web/src/lib/goals/goal.lib.test.ts
+++ web/src/lib/goals/goal.lib.test.ts
@@ -10,26 +10,26 @@ describe("toRecentlyExpiredGoalEntries", () => {
   it("should return goals that expired within the last hour", () => {
     vi.setSystemTime(
       DateUtils.dayjs
-        .tz("2024-03-20 03:00:00.000", "America/Los_Angeles")
+        .tz("2024-03-20 00:00:00.000", "America/Los_Angeles")
         .toDate(),
     );
 
     const allGoals = [
       {
         dueAt: DateUtils.dayjs
-          .tz("2024-03-20 02:30:00.000", "America/Los_Angeles")
+          .tz("2024-03-19 23:59:59.999", "America/Los_Angeles")
           .toDate(),
         userTimezone: "America/Los_Angeles",
       },
       {
         dueAt: DateUtils.dayjs
-          .tz("2024-03-20 02:15:00.000", "America/Los_Angeles")
+          .tz("2024-03-19 23:30:00.000", "America/Los_Angeles")
           .toDate(),
         userTimezone: "America/Los_Angeles",
       },
       {
         dueAt: DateUtils.dayjs
-          .tz("2024-03-20 02:00:00.001", "America/Los_Angeles")
+          .tz("2024-03-19 23:00:00.001", "America/Los_Angeles")
           .toDate(),
         userTimezone: "America/Los_Angeles",
       },
@@ -37,13 +37,13 @@ describe("toRecentlyExpiredGoalEntries", () => {
       // exactly one hour ago is the boundary
       {
         dueAt: DateUtils.dayjs
-          .tz("2024-03-20 02:00:00.000", "America/Los_Angeles")
+          .tz("2024-03-19 23:00:00.000", "America/Los_Angeles")
           .toDate(),
         userTimezone: "America/Los_Angeles",
       },
       {
         dueAt: DateUtils.dayjs
-          .tz("2024-03-20 01:59:59.999", "America/Los_Angeles")
+          .tz("2024-03-19 22:59:59.999", "America/Los_Angeles")
           .toDate(),
         userTimezone: "America/Los_Angeles",
       },
@@ -59,14 +59,14 @@ describe("toRecentlyExpiredGoalEntries", () => {
   it("should exclude goals that expired more than an hour ago", () => {
     vi.setSystemTime(
       DateUtils.dayjs
-        .tz("2024-03-20 03:00:00.000", "America/Los_Angeles")
+        .tz("2024-03-20 00:00:00.000", "America/Los_Angeles")
         .toDate(),
     );
 
     const goals = [
       {
         dueAt: DateUtils.dayjs
-          .tz("2024-03-20 01:30:00.000", "America/Los_Angeles")
+          .tz("2024-03-19 22:30:00.000", "America/Los_Angeles")
           .toDate(),
         userTimezone: "America/Los_Angeles",
       },
@@ -79,14 +79,14 @@ describe("toRecentlyExpiredGoalEntries", () => {
   it("should exclude future goals", () => {
     vi.setSystemTime(
       DateUtils.dayjs
-        .tz("2024-03-20 03:00:00.000", "America/Los_Angeles")
+        .tz("2024-03-20 00:00:00.000", "America/Los_Angeles")
         .toDate(),
     );
 
     const goals = [
       {
         dueAt: DateUtils.dayjs
-          .tz("2024-03-20 03:30:00.000", "America/Los_Angeles")
+          .tz("2024-03-20 00:30:00.000", "America/Los_Angeles")
           .toDate(),
         userTimezone: "America/Los_Angeles",
       },
@@ -96,23 +96,45 @@ describe("toRecentlyExpiredGoalEntries", () => {
     expect(result).toHaveLength(0);
   });
 
-  // it("should handle different timezones correctly", () => {
-  //   vi.setSystemTime(
-  //     DateUtils.dayjs
-  //       .tz("2024-03-20 03:00:00.000", "America/Los_Angeles")
-  //       .toDate(),
-  //   );
+  it("handles left-side timezones correctly", () => {
+    vi.setSystemTime(
+      DateUtils.dayjs
+        .tz("2024-03-20 00:00:00.000", "America/Los_Angeles")
+        .toDate(),
+    );
 
-  //   const goals = [
-  //     {
-  //       dueAt: DateUtils.dayjs
-  //         .tz("2024-03-20 02:30:00.000", "America/New_York")
-  //         .toDate(),
-  //       userTimezone: "America/New_York",
-  //     },
-  //   ] as CompleteGoalEntry[];
+    const goals = [
+      {
+        dueAt: DateUtils.dayjs
+          // this is 20:59 PST. so it didn't expire recently
+          .tz("2024-03-19 23:59:59.999", "America/New_York")
+          .toDate(),
+        userTimezone: "America/New_York",
+      },
+    ] as CompleteGoalEntry[];
 
-  //   const result = toRecentlyExpiredGoalEntries(goals);
-  //   expect(result).toHaveLength(1);
-  // });
+    const result = toRecentlyExpiredGoalEntries(goals);
+    expect(result).toHaveLength(0);
+  });
+
+  it("handles right-side timezones correctly", () => {
+    vi.setSystemTime(
+      DateUtils.dayjs
+        .tz("2024-03-20 00:00:00.000", "America/New_York")
+        .toDate(),
+    );
+
+    const goals = [
+      {
+        dueAt: DateUtils.dayjs
+          // this is 2024-03-20 2:59 EST. so it didn't expire recently
+          .tz("2024-03-19 23:59:59.999", "America/Los_Angeles")
+          .toDate(),
+        userTimezone: "America/New_York",
+      },
+    ] as CompleteGoalEntry[];
+
+    const result = toRecentlyExpiredGoalEntries(goals);
+    expect(result).toHaveLength(0);
+  });
 });
